31f60d802eb438eadf2ecb240f867eefe801ef02
Novais <abigail62810@gmail.com>
2024-01-15 12:12:07 +0100

    aezr

--- src/Data/data.js
+++ src/Data/data.js
@@ -1,10 +1,7 @@
 $myTasks = [
     { id: 1, name: "Miata" },
     { id: 2, name: "Subaru" },
-    { id: 3, name: "Supra" }
-    
+    { id: 3, name: "Supra" } 
 ];
-
-console.log($myTasks);
 
 export { myTasks };
