3bd563ffa2fdf68010843b66b2c93b95c62641d1
Nathan COQUART <coquartnathan@gmail.com>
2024-01-22 11:55:32 +0100

    le texte lo

--- src/App/index.js
+++ src/App/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TodoList from '../TodoList'; 
 import { myTasks } from '../Data/data';
 import styles from "./App.module.css";
@@ -11,14 +11,36 @@ function App() {
     console.log(`Double clic sur la tâche : ${task.name}`);
   };
 
+  
+  const [name,setName] = useState("");
+
+  function handleChange(event){
+  setName(event.target.value);
+  }
+  
+  function handleSubmit(event){
+  event.preventDefault(); 
+  }
+  
+
   return (
     <div className="App">
       <h1>Liste des tâches</h1>
       <h2 className={styles.title}>Sous titre</h2>
 
       <TodoList myTasks={myTasks} handleDoubleClick={handleDoubleClick} /* on charge l'info du double clique*//> 
+
+
+      <form onSubmit={handleSubmit}>
+    <label htmlFor="name">met tin texte lo</label>
+    <input type="input" onChange={handleChange} value={name}/>
+    <button type="submit">OK</button>
+    </form>
+      
     </div>
   );
 }
+
+
 
 export default App;
