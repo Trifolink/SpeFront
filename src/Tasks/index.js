e99c07bf5493253bf78ae2e7362a1819d1768ae1
Novais <abigail62810@gmail.com>
2024-01-29 09:19:00 +0100

    Ca fonctionne la barre

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,28 +1,22 @@
 import React, { useState } from 'react';
 
+export default function Task({ name, onDoubleClick, isBlocked }) {
+  const [counter, setCounter] = useState(true);
 
-// export default function Task(props){
-// // console.log("props=",props)
-//     return(
-//         <li>{props.name}</li>
-//     )
-// }
+  const handleClick = () => {
+    if (!isBlocked) {
+      setCounter(!counter);
+      onDoubleClick();  // Appel de la fonction onDoubleClick uniquement si la tâche n'est pas bloquée
+    }
+  };
 
-export default function Task({ name, onDoubleClick }) {
-    const [counter, setCounter] = useState(true);
-  
-    const handleClick = () => {
-        setCounter(!counter);
-    };
-  
-    const taskStyle = {
-      textDecoration: counter ? 'none' : 'line-through'
-    };
-  
-    return (
-      <li onClick={handleClick} onDoubleClick={onDoubleClick} style={taskStyle}>
-        {name}
-      </li>
-    )
-};
+  const taskStyle = {
+    textDecoration: counter ? 'none' : 'line-through'
+  };
 
+  return (
+    <li onClick={handleClick} onDoubleClick={onDoubleClick} style={taskStyle}>
+      {name}
+    </li>
+  );
+}
